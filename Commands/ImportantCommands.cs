5d370fa58d66a230a54d479067266d2c33278f38
Home Data <cybertekcx001@gmail.com>
2020-01-30 12:09:43 +0530

    Importing project

---
+++ Commands/ImportantCommands.cs
@@ -0,0 +1,21 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatusBot.Commands
+{
+    public class ImportantCommands : BaseCommandModule
+    {
+        [Command("status")]
+        public async Task Status(CommandContext context, string message)
+        {
+            if (message.ToLower() == "kitsune")
+            {
+                await context.Channel.SendMessageAsync("Detected").ConfigureAwait(false);
+            }
+        }
+    }
+}
